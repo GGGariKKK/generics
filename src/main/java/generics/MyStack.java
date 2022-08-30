5070dc1c83d5eaa368b6a73810ca4ccb8b28349e
Illia Harkusha <harkusha.kp13@gmail.com>
2022-08-30 09:39:17 +0300

    Implemented MyStack

---
+++ src/main/java/generics/MyStack.java
@@ -0,0 +1,61 @@
+package generics;
+
+import java.util.Arrays;
+import java.util.EmptyStackException;
+import java.util.StringJoiner;
+
+public class MyStack<T> {
+    private static final int DEFAULT_SIZE = 3;
+    private Object[] stack;
+    private int pointer;
+
+    public MyStack() {
+        stack = new Object[DEFAULT_SIZE];
+        pointer = -1;
+    }
+
+    public void push(T value) {
+        if (pointer + 1 >= stack.length)
+            stack = Arrays.copyOf(stack, stack.length * 2);
+        stack[++pointer] = value;
+    }
+
+    public boolean remove(T value) {
+        for (int i = 0; i <= pointer; i++)
+            if (stack[i] == value) {
+                System.arraycopy(stack, i + 1, stack, i, stack.length - i - 1);
+                pointer--;
+                return true;
+            }
+        return false;
+    }
+
+    public void clear(){
+        stack = new Object[DEFAULT_SIZE];
+        pointer = -1;
+    }
+
+    public int size(){
+        return pointer + 1;
+    }
+
+    public T peek(){
+        if(pointer == -1)
+            throw new EmptyStackException();
+        return (T) stack[pointer];
+    }
+
+    public T pop(){
+        if(pointer == -1)
+            throw new EmptyStackException();
+        return (T) stack[pointer--];
+    }
+
+    @Override
+    public String toString() {
+        var res = new StringJoiner(", ", "[", "]");
+        for (int i = 0; i <= pointer; i++)
+            res.add(stack[i].toString());
+        return res.toString();
+    }
+}
