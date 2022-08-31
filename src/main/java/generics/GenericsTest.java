455899fb7eae1031ac07b71c1ee2a2e8a996edfa
Illia Harkusha <harkusha.kp13@gmail.com>
2022-08-31 19:02:23 +0300

    Wrote arrayListTest

--- src/main/java/generics/GenericsTest.java
+++ src/main/java/generics/GenericsTest.java
@@ -2,6 +2,27 @@ package generics;
 
 public class GenericsTest {
     public static void main(String[] args) {
+        myArrayListTest();
+    }
 
+    public static void myArrayListTest(){
+        var arList = new MyArrayList<String>();
+        arList.add("a");
+        arList.add("b");
+        arList.add("c");
+
+        System.out.println(arList);
+
+        arList.remove(1);
+
+        System.out.println(arList);
+
+        System.out.println(arList.get(1));
+
+        arList.clear();
+
+        System.out.println(arList);
+
+        System.out.println(arList.size());
     }
 }
